1d65ce783be41943e5d6303c78bc483daecf2b7f
santossofiaana <sofia.santos.ana@escola.pr.gov.br>
2026-06-22 12:14:27 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ function back() {
      var resultado=document.getElementById('resultado').innerHTML;
      document.getElementById('resultado'). innerHTML=resultado. substring(0,resultado.length-1);
 }
+function calcular() {
+    var resultado=document.getElementById('resultado').innerHTML;
+    if (resultado) {
+        document. getElementById('resultado'). innerHTML=eval(resultado);
+    }
+    else{
+        document. getElementById('resultado'). innerHTML="nada para calcular";
+    }
+}
